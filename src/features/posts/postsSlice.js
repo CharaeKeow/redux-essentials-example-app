4ea72c6eaa45c24d1832905392985758d817713e
CharaeKeow <charaekeow11@gmail.com>
2022-03-02 03:41:21 +0800

    change the id generation to inside prepare()

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
   { id: '1', title: 'First Posts!', content: 'Hello!' },
@@ -9,14 +9,35 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      //push the payload - form data - into the state array. Despite this is mutating the state,
-      //it is valid in createSlice due to it uses Immer. Don't use this outside createSlice though
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        //push the payload - form data - into the state array. Despite this is mutating the state,
+        //it is valid in createSlice due to it uses Immer. Don't use this outside createSlice though
+        state.push(action.payload)
+      },
+      //create the id inside reducer so that the component would only need to send the title and content
+      //payload. Also prevent duplication
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          }
+        }
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload //assign variable according to payload
+      const existingPost = state.find(post => post.id === id) //find the state in store
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
     }
   }
 })
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, postUpdated } = postsSlice.actions
 
 export default postsSlice.reducer
